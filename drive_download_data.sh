ac2be6d6176fd99ea0ed41775de387fe665849b7
AliciaBentley-NOAA <alicia.bentley@noaa.gov>
2020-06-04 21:02:23 +0000

    Successfully downloaded GFS analysis files. Will need to change the date of the latest naming convention change after I find out what it is.

---
+++ drive_download_data.sh
@@ -0,0 +1,62 @@
+#!/bin/bash
+# Author: Alicia Bentley
+# Modified: 4 June 2020
+#
+###################################################
+# Script to get download necessary data from HPSS #
+###################################################
+
+#==============================================  BEGIN CHANGES  ================================================
+
+export SCRIPTS_PATH='/scratch2/NCEPDEV/stmp3/Alicia.Bentley/scripts/gfsv16'
+export DATA_PATH='/scratch2/NCEPDEV/stmp3/Alicia.Bentley/scripts/gfsv16'
+
+export CASE='cristobal'
+export FHR_START=0
+export FHR_END=24
+export FHR_INC=6
+
+export GET_RET=false
+export GET_GFS=true
+export GET_RAP=false
+export GET_ST4=false
+
+for cycle in 2020060100
+#for cycle in 2017082200 2017082300 2017082400 2017082500 2017082600 2017082700 2017082800 2017082900 2017083000 2017083100 2017090100
+do
+
+export CYCLE=${cycle}
+
+#===============================================  END CHANGES  =================================================
+
+if [ $GET_RET = true ]; then
+   echo "Create/submit script to download ${CYCLE} GFSv16 RETRO and OPS GFS forecast data"
+   ${SCRIPTS_PATH}/create_htar_retros.sh
+   sleep 5
+fi
+
+
+if [ $GET_GFS = true ]; then
+   echo "Create/submit script to download GFS analysis data"
+   ${SCRIPTS_PATH}/create_htar_gfs.sh
+   sleep 5
+fi
+
+
+if [ $GET_RAP = true ]; then
+   echo "Create/submit script to download RAP analysis data"
+   ${SCRIPTS_PATH}/create_htar_rap.sh
+   sleep 5
+fi
+
+
+if [ $GET_ST4 = true ]; then
+   echo "Create/submit script to download Stage-IV analysis data"
+   ${SCRIPTS_PATH}/create_htar_st4.sh
+   sleep 5
+fi
+
+
+done
+
+exit
