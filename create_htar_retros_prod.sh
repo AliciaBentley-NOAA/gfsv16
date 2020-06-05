30f496576118f1beb5bc97fff0205ce4889e3e42
AliciaBentley-NOAA <alicia.bentley@noaa.gov>
2020-06-05 21:29:47 +0000

    Scripts to submit jobs to download GFS analyses and GFSv15/GFSv16 forecasts are working and up to date.

---
+++ create_htar_retros_prod.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+# Author: Alicia Bentley
+#####################################
+# Script for submitting jobs on Hera
+#   that download data from HPSS
+#####################################
+
+echo path: ${DATA_PATH}/${CASE}/${CYCLE}
+echo fhr_inc: ${FHR_INC}
+echo fhr_start: ${FHR_START}
+echo fhr_end: ${FHR_END}
+
+mkdir -p ${DATA_PATH}/${CASE}/${CYCLE}
+'cp' ${SCRIPTS_PATH}/htar_retros_prod.ncl ${DATA_PATH}/${CASE}/${CYCLE}/.
+cd ${DATA_PATH}/${CASE}/
+
+cat > ${DATA_PATH}/${CASE}/${CYCLE}/htar_retros_prod.csh <<EOF
+#!/bin/csh
+#SBATCH --account=ovp
+#SBATCH --job-name=v16_htar
+#SBATCH --output=retros_prod_htar.%j.out
+#SBATCH --ntasks=1
+#SBATCH --partition=service
+#SBATCH --time=8:00:00
+
+cd ${DATA_PATH}/${CASE}/${CYCLE}/
+
+/bin/rm -rf ../htar_retros_prod_${CYCLE}_done
+
+echo ncl 'scriptyyyymmddhh="${CYCLE}"' 'eventname="${CASE}"' 'fhr_inc="${FHR_INC}"' 'fhr_start="${FHR_START}"' 'fhr_end="${FHR_END}"' ${DATA_PATH}/${CASE}/${CYCLE}/htar_retros_prod.ncl 
+
+ncl 'scriptyyyymmddhh="${CYCLE}"' 'eventname="${CASE}"' 'fhr_inc="${FHR_INC}"' 'fhr_start="${FHR_START}"' 'fhr_end="${FHR_END}"' ${DATA_PATH}/${CASE}/${CYCLE}/htar_retros_prod.ncl
+#sleep 5
+
+touch ../htar_retros_prod_${CYCLE}_done
+
+exit
+
+EOF
+
+
+
+sbatch ${DATA_PATH}/${CASE}/${CYCLE}/htar_retros_prod.csh
+sleep 5
+
+exit
+
--- drive_download_data.sh
+++ drive_download_data.sh
@@ -11,17 +11,17 @@
 export SCRIPTS_PATH='/scratch2/NCEPDEV/stmp3/Alicia.Bentley/scripts/gfsv16'
 export DATA_PATH='/scratch2/NCEPDEV/stmp3/Alicia.Bentley/scripts/gfsv16'
 
-export CASE='cristobal'
+export CASE='test'
 export FHR_START=0
-export FHR_END=24
+export FHR_END=12
 export FHR_INC=6
 
-export GET_RET=false
-export GET_GFS=true
+export GET_RET=true
+export GET_ANL=true
 export GET_RAP=false
 export GET_ST4=false
 
-for cycle in 2020060100
+for cycle in 2019090312
 #for cycle in 2017082200 2017082300 2017082400 2017082500 2017082600 2017082700 2017082800 2017082900 2017083000 2017083100 2017090100
 do
 
@@ -31,14 +31,14 @@ export CYCLE=${cycle}
 
 if [ $GET_RET = true ]; then
    echo "Create/submit script to download ${CYCLE} GFSv16 RETRO and OPS GFS forecast data"
-   ${SCRIPTS_PATH}/create_htar_retros.sh
+   ${SCRIPTS_PATH}/create_htar_retros_prod.sh
    sleep 5
 fi
 
 
-if [ $GET_GFS = true ]; then
+if [ $GET_ANL = true ]; then
    echo "Create/submit script to download GFS analysis data"
-   ${SCRIPTS_PATH}/create_htar_gfs.sh
+   ${SCRIPTS_PATH}/create_htar_gfs_anl.sh
    sleep 5
 fi
 
